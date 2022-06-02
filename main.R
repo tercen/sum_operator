5e8b177b61b048fd354560fad825457990c882d7
agouy <contact@agouy.com>
2022-06-02 09:12:16 +0000

    optimisation + test

--- main.R
+++ main.R
@@ -1,11 +1,14 @@
 library(tercen)
 library(dplyr)
+library(dtplyr)
 
 ctx <- tercenCtx()
 
 ctx %>% 
   select(.y, .ci, .ri) %>% 
+  lazy_dt() %>%
   group_by(.ci, .ri) %>%
   summarise(sum = sum(.y)) %>%
+  as_tibble() %>%
   ctx$addNamespace() %>%
   ctx$save()
