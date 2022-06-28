b5446079f7acfc5cad414be8c6a5539d21b0b30a
lucas501 <lucas.lelann@gmail.com>
2022-06-28 12:38:18 +0000

    test for realease

--- main.R
+++ main.R
@@ -2,13 +2,22 @@ library(tercen)
 library(dplyr)
 library(dtplyr)
 
+#library(tim)
+#options("tercen.workflowId" = "78e8fce3c8d25341e1a5ee610e00571f")
+#options("tercen.stepId"     = "3acc3418-c743-418d-8274-fcf2ae66dae2")
+
 ctx <- tercenCtx()
 
-ctx %>% 
+df_out<- ctx %>% 
   select(.y, .ci, .ri) %>% 
   lazy_dt() %>%
   group_by(.ci, .ri) %>%
   summarise(sum = sum(.y)) %>%
   as_tibble() %>%
-  ctx$addNamespace() %>%
+  ctx$addNamespace()
+
+df_out %>%
   ctx$save()
+
+#build_test_data(df_out, ctx, test_name = "test1", test_folder = "./tests")
+
