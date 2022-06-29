cc9bd22974e6ede9e521b2990bbd4df8595f6339
Alexandre Gouy <contact@agouy.com>
2022-06-29 09:20:38 +0200

    cleaning

--- main.R
+++ main.R
@@ -2,10 +2,6 @@ library(tercen)
 library(dplyr)
 library(dtplyr)
 
-#library(tim)
-#options("tercen.workflowId" = "78e8fce3c8d25341e1a5ee610e00571f")
-#options("tercen.stepId"     = "3acc3418-c743-418d-8274-fcf2ae66dae2")
-
 ctx <- tercenCtx()
 
 df_out<- ctx %>% 
@@ -18,6 +14,3 @@ df_out<- ctx %>%
 
 df_out %>%
   ctx$save()
-
-#build_test_data(df_out, ctx, test_name = "test1", test_folder = "./tests")
-
